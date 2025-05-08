226b4daf0fa77ef1dfd2e7b09f317d5c877573ab
Tobias Alves <tobiasjose12@gmail.com>
2025-05-08 13:14:06 -0300

    up

---
+++ projeto/exercicio-1/src/App.jsx
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div>
+      <img src="public\react.png" alt="" />
+      <h1>React</h1>
+      <p>A biblioteca para interface de usuário web e nativas</p>
+      <button>Aprenda React</button>
+      <button>API</button>
+      <hr />
+      <h2>Crie interfaces de usuário de componentes</h2>
+      <p>
+        React permite que você construa interfaces de usuário a partir de
+        pedaços individuais chamados componentes.
+      </p>
+      <hr />
+      <h2>Escreva componentes com código e marcação</h2>
+      <p>
+        Componentes React são funções JavaScript. A sintaxe de marcação é
+        chamada de JSX. É uma extensão da sintaxe JavaScript popularizada pelo
+        React.
+      </p>
+      <hr />
+      <h2>Próximos passos</h2>
+      <ul>
+        <li>Uso de dados dinâmicos no JSX</li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos do JavaScript</li>
+      </ul>
+    </div>
+  );
+}
