e2b8c80da4b1ed7d09be9e969d51683bb8ce86de
Tobias Alves <tobiasjose12@gmail.com>
2025-05-09 12:42:30 -0300

    Update App.jsx

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -1,6 +1,7 @@
 import Title from "./components/Title";
 import SubTitle from "./components/SubTitle";
 import Status from "./components/Status";
+import styles from "./App.module.css";
 
 function sum(a, b) {
   return a + b;
@@ -8,7 +9,7 @@ function sum(a, b) {
 
 export default function App() {
   return (
-    <div>
+    <div className={styles.app}>
       <Title />
       <SubTitle />
       <Status />
