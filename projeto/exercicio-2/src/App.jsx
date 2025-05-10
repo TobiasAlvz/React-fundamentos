badaeb8dcc9487818f4ff044ee057bcf500e95d4
Tobias Alves <tobiasjose12@gmail.com>
2025-05-10 11:59:12 -0300

    Update App.jsx

--- projeto/exercicio-2/src/App.jsx
+++ projeto/exercicio-2/src/App.jsx
@@ -1,14 +1,18 @@
-
 import Card from "./components/card";
+import image1 from "./assets/ba9464145eba8762f6286a3c8387c951.jpg";
+import image2 from "./assets/esb-poster.jpg";
+import image3 from "./assets/rotj-poster.jpg";
+import Button from "./components/Button";
 
 function App() {
   return (
     <>
+    <Button text="AAAAAAAAAAAAAAAAAAAAAAAAAAAA"/>
       <h1>Exercicio 2</h1>
-      <Card />
-      <Card />
-      <Card />
-  
+      <Card title="aaaaaaaaaaaaaaaaaaaaaaa" poster={image1} />
+      <Card title="bbbbbbbbbbbbbbbbbbbbbbb" poster={image2} />
+      <Card title="ccccccccccccccccc" poster={image3} />
+      <Button text="Comprar Agora"/>
     </>
   );
 }
