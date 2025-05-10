1a7dc22d1baf50400fcc69c9b50dd11e2009f635
Tobias Alves <tobiasjose12@gmail.com>
2025-05-10 11:59:25 -0300

    Update Card.jsx

--- projeto/exercicio-2/src/components/Card.jsx
+++ projeto/exercicio-2/src/components/Card.jsx
@@ -1,27 +1,27 @@
 import styles from "./card.module.css";
 import Button from "./Button";
 
-export default function Card() {
+export default function Card({title, poster}) {
   return (
     <div className={styles.container}>
       {" "}
       <div className={styles.card}>
         <div className={styles.imageContainer}>
           <img
-            src="/ba9464145eba8762f6286a3c8387c951.jpg"
-            alt="Pôster de Star Wars"
+            src={poster}
+            alt={title}
             className={styles.poster}
           />
         </div>
         <div className={styles.content}>
-          <h2 className={styles.title}>Pôster: Star Wars (1977)</h2>
+          <h2 className={styles.title}>{title}</h2>
           <p className={styles.description}>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea illum
             est sequi ut, odit quasi cumque corrupti voluptatem, modi iste
             commodi velit nostrum itaque obcaecati earum dolores. Similique, a
             delectus!
           </p>
-          <Button />
+          <Button text="Comprar Agora"/>
         </div>
       </div>
     </div>
