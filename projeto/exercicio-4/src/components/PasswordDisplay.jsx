d6dd359433ec7bd1391193c7f77405fbe55c3261
Tobias Alves <tobiasjose12@gmail.com>
2025-05-13 23:03:34 -0300

    Update PasswordDisplay.jsx

--- projeto/exercicio-4/src/components/PasswordDisplay.jsx
+++ projeto/exercicio-4/src/components/PasswordDisplay.jsx
@@ -1,6 +1,7 @@
 import styles from './PasswordDisplay.module.css';
 
 export default function PasswordDisplay({ senha }) {
+  
   if (!senha) return null;
   return <p className={styles.senha}>{senha}</p>;
 }
