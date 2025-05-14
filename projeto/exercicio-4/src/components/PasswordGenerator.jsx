fa8942133fb30ef00279fd5418cf97a6c54de069
Tobias Alves <tobiasjose12@gmail.com>
2025-05-13 22:48:01 -0300

    Update PasswordGenerator.jsx

--- projeto/exercicio-4/src/components/PasswordGenerator.jsx
+++ projeto/exercicio-4/src/components/PasswordGenerator.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react';
-import PasswordDisplay from './PasswordDisplay';
-import CopyButton from './CopyButton';
-import styles from './PasswordGenerator.module.css';
-
-function gerarSenha(tamanho = 12) {
-  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*()_-=+';
-  let senha = '';
-  for (let i = 0; i < tamanho; i++) {
-    const index = Math.floor(Math.random() * caracteres.length);
-    senha += caracteres[index];
-  }
-  return senha;
-}
+import { useState } from "react";
+import PasswordDisplay from "./PasswordDisplay";
+import CopyButton from "./CopyButton";
+import styles from "./PasswordGenerator.module.css";
 
 export default function PasswordGenerator() {
-  const [senha, setSenha] = useState('');
+  const [senha, setSenha] = useState("");
   const [copiado, setCopiado] = useState(false);
+  const [passwordSize, setPasswordSize] = useState(12);
+  function gerarSenha() {
+    const caracteres =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*()_-=+";
+    let senha = "";
+    for (let i = 0; i < passwordSize; i++) {
+      const index = Math.floor(Math.random() * caracteres.length);
+      senha += caracteres[index];
+    }
+    return senha;
+  }
 
   const gerar = () => {
-    const nova = gerarSenha();
+    const nova = gerarSenha(Number(passwordSize));
     setSenha(nova);
     setCopiado(false);
   };
@@ -31,7 +32,16 @@ export default function PasswordGenerator() {
   return (
     <>
       <div className={styles.buttons}>
-        <button className={styles.button} onClick={gerar}>Gerar!</button>
+        <input
+          type="number"
+          id="passwordSize"
+          min={1}
+          value={passwordSize}
+          onChange={(ev) => setPasswordSize(ev.target.value)}
+        />
+        <button className={styles.button} onClick={gerar}>
+          Gerar!
+        </button>
         <CopyButton onClick={copiar} copiado={copiado} disabled={!senha} />
       </div>
       <PasswordDisplay senha={senha} />
