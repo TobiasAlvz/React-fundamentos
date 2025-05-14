57673345b37b44dce4a02ff6314dcad3e6df35a8
Tobias Alves <tobiasjose12@gmail.com>
2025-05-13 22:48:40 -0300

    Delete PasswordDisplay.jsx

--- projeto/exercicio-4/src/components/PasswordDisplay.jsx
+++
@@ -1,6 +0,0 @@
-import styles from './PasswordDisplay.module.css';
-
-export default function PasswordDisplay({ senha }) {
-  if (!senha) return null;
-  return <p className={styles.senha}>{senha}</p>;
-}
